80673db3f57acba9562b59a28ba5fec3b4df38f7
adr1anhz <addriaan@proton.me>
2025-07-11 19:22:46 +0200

    Initial Commit

---
+++ tests/test_math2.py
@@ -0,0 +1,19 @@
+# Multiplication test ideas:
+# two positive integers
+# identity: multiply by 1
+# zero: multiply by 0
+# positive by negative
+# negative by negative
+# floats
+
+
+def test_multiply_two_positive_inits():
+    assert 2 * 3 == 6
+    
+
+def test_multiply_identity():
+    assert 1 * 99 == 99
+
+
+def test_multiply_zero():
+    assert 0 * 100 == 100
