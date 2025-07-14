f758398edc11c0a42648a48bd49063d5d79dd288
adr1anhz <addriaan@proton.me>
2025-07-14 12:27:45 +0200

    API Testing

---
+++ tests/test_api.py
@@ -0,0 +1,23 @@
+import pytest
+import requests
+
+
+@pytest.mark.duckduckgo
+@pytest.mark.api
+def test_duckduckgo_instant_answer_api():
+    
+    # Arrange
+    url = "https://api.duckduckgo.com/?q=python+programming&format=json"
+
+    # Act
+    response = requests.get(url)
+    body = response.json()
+    
+
+    # Assert
+    assert response.status_code in [200, 202]
+    assert 'Python' in body['AbstractText']
+
+    print(body)
+
+test_duckduckgo_instant_answer_api()
