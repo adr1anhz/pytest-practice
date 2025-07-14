4df46b0062db8617e297b982f4db0f1ed6358b85
adr1anhz <addriaan@proton.me>
2025-07-14 12:06:01 +0200

    Filtering Tests

--- tests/test_accum.py
+++ tests/test_accum.py
@@ -4,30 +4,35 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import pytest
 from stuff.accum import Accumulator
 
-@pytest.fixture
+@pytest.mark.accumulator
 def accum():
     return Accumulator()
 
-
+@pytest.mark.accumulator
 def test_accumulator_init(accum, accum2):
     assert accum.count == 0
 
+
+@pytest.mark.accumulator
 def test_accumulator_add_one():
     accum = Accumulator()
     accum.add()
     assert accum.count == 1
 
+@pytest.mark.accumulator
 def test_accumulator_add_three():
     accum = Accumulator()
     accum.add(3)
     assert accum.count == 3
 
+@pytest.mark.accumulator
 def test_accumulator_add_twice():
     accum = Accumulator()
     accum.add()
     accum.add()
     assert accum.count == 2
 
+@pytest.mark.accumulator
 def test_accumulator_cannot_set_count_directly():
     accum = Accumulator()
     with pytest.raises(AttributeError, match=r"property 'count' of 'Accumulator' object has no setter"):
